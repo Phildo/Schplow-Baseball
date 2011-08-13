5281cd6a7aecaff497d3b6fb35c4bc3ab678d85a
Brian Thiel <brthiel@wisc.edu>
2011-08-12 22:13:16 -0500

    the rest of it

---
+++ Schplow Baseball/HelloWorldLayer.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldLayer.h
+//  Schplow Baseball
+//
+//  Created by Philip Dougherty on 8/11/11.
+//  Copyright UW Madison 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+#import "Box2D.h"
+#import "GLES-Render.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+	b2World* world;
+	GLESDebugDraw *m_debugDraw;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+// adds a new sprite at a given coordinate
+-(void) addNewSpriteWithCoords:(CGPoint)p;
+
+@end
---
+++ Schplow Baseball/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Schplow Baseball
+//
+//  Created by Philip Dougherty on 8/11/11.
+//  Copyright UW Madison 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
