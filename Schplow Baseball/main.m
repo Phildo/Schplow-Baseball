46a3cdd16c216174222d5189f915969ea47d5ee3
Brian Thiel <brthiel@wisc.edu>
2011-08-12 22:11:01 -0500

    xcode junk

---
+++ Schplow Baseball/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Schplow Baseball
+//
+//  Created by Philip Dougherty on 8/11/11.
+//  Copyright UW Madison 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
